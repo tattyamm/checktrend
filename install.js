65700d65e5591e5cf314ed09e5a3e37286f4fcc5
tattyamm <entatsu.appli+github@gmail.com>
2013-09-25 00:04:44 +0900

    Firefox OS向けインストール用jsを修正

--- install.js
+++ install.js
@@ -1,11 +1,18 @@
 
 // ホスト型アプリのインストール
   function install(ev) {
-    ev.preventDefault();
     // マニフェスト URL を定義
-    var manifest_url = "http://tattyamm.github.io/checktrend/manifest.webapp";
     // アプリをインストール
-    var myapp = navigator.mozApps.install(manifest_url);
+    if(ev=="install"){
+      var manifest_url = "http://tattyamm.github.io/checktrend/manifest.webapp";
+      var myapp = navigator.mozApps.install(manifest_url);
+      console.log("install");
+    }
+    if(ev=="installPackage"){
+      var manifest_url = "http://tattyamm.github.io/checktrend/package.webapp";
+      var myapp = navigator.mozApps.installPackage(manifest_url);
+      console.log("installPackage");
+    }
     myapp.onsuccess = function(data) {
       // アプリがインストールされたらボタンを削除
       this.parentNode.removeChild(this);
@@ -15,28 +22,3 @@
       console.log('インストール失敗、エラー: ' + this.error.name);
      };
   };
-
-  // ボタンへの参照を取得し、クリック時に install() を呼び出します
-  var button = document.getElementById('install');
-  button.addEventListener('click', install, false);
-
-
-  function installPackage(ev) {
-    ev.preventDefault();
-    // マニフェスト URL を定義
-    var manifest_url = "http://tattyamm.github.io/checktrend/package.webapp";
-    // アプリをインストール
-    var myapp = navigator.mozApps.installPackage(manifest_url);
-    myapp.onsuccess = function(data) {
-      // アプリがインストールされたらボタンを削除
-      this.parentNode.removeChild(this);
-    };
-    myapp.onerror = function() {
-      // アプリがインストールされなかった場合、this.error.name に情報が含まれています
-      console.log('インストール失敗、エラー: ' + this.error.name);
-     };
-  };
-  // ボタンへの参照を取得し、クリック時に install() を呼び出します
-  var button2 = document.getElementById('installPackage');
-  button2.addEventListener('click', installPackage, false);
-
